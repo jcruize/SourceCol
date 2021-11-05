9cb04ccfa24b3cff07ca8b28b4a1e6deb2ea3ec3
Juan Camilo Ruiz <juan.camilo.ruiz@hotmail.com>
2021-11-04 22:59:59 -0500

    Punto 3 funcionando correctamente

--- SourceCol/Program.cs
+++ SourceCol/Program.cs
@@ -10,6 +10,27 @@ namespace SourceCol
     {
         static void Main(string[] args)
         {
+            //PUNTO 3
+            int[] numArray = { 0, 10, 32, 58, 45, 7, 26 };
+
+            (int[] list, int countPair) orderedArray = orderArray(numArray); 
+
+            Console.WriteLine("La cantidad total de números es: {0}, y están ordenados de mayor a menor de la siguiente manera: ", orderedArray.countPair);
+            Array.ForEach(numArray, Console.WriteLine);
+            Console.ReadLine();
+        }
+
+        private static (int[], int) orderArray(int[] numArray)
+        {
+                       
+            Array.Sort(numArray);
+            Array.Reverse(numArray);
+            int totalCount = numArray.Count();
+            return (numArray, totalCount);
         }
     }
+
+
+
+
 }

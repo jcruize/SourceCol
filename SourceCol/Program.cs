4cb2b479318ac07a860d036358ad05b8ba50f4e3
Juan Camilo Ruiz <juan.camilo.ruiz@hotmail.com>
2021-11-05 21:44:59 -0500

    Organización de puntos corregida

--- SourceCol/Program.cs
+++ SourceCol/Program.cs
@@ -11,6 +11,31 @@ namespace SourceCol
         static void Main(string[] args)
         {
 
+            // PUNTO 1
+            // Se pide al usuario la cadena de texto
+            Console.WriteLine("Ingrese una cadena de texto: ");
+            string completeText = Console.ReadLine();
+
+            // Se solicita la cántidad máxima de caracteres para realizar el recorte, si no se da ningún valor, por defect es 20
+            Console.WriteLine("Ingrese una cantidad máxima de caracteres(20 por defecto): ");
+            int maxChars = int.TryParse(Console.ReadLine(), out maxChars) ? maxChars : 20;
+
+            string trimText = TrimString(completeText, maxChars);
+            Console.WriteLine(trimText);
+
+            Console.ReadLine();
+
+
+            //PUNTO 2
+            //Se pide número a validar, por defecto se pone 1, aunque se podría validar ingreso del usuario
+            Console.WriteLine("Ingrese un entero para validar si cumple con la sucesión de Fibonacci (1 por defecto): ");
+            int num = int.TryParse(Console.ReadLine(), out num) ? num : 1;
+
+            string validateFibonacci = isFibonacci(num);
+            Console.WriteLine(validateFibonacci);
+            Console.ReadLine();
+
+
             //PUNTO 3
             int[] numArray = { 0, 10, 32, 58, 45, 7, 26 };
 
@@ -19,6 +44,7 @@ namespace SourceCol
             Console.WriteLine("La cantidad total de números es: {0}, y están ordenados de mayor a menor de la siguiente manera: ", orderedArray.countPair);
             Array.ForEach(numArray, Console.WriteLine);
             Console.ReadLine();
+
         }
 
         private static (int[], int) orderArray(int[] numArray)
@@ -27,42 +53,14 @@ namespace SourceCol
             Array.Sort(numArray);
             Array.Reverse(numArray);
             int totalCount = numArray.Count();
-            return (numArray, totalCount);
-
-            //PUNTO 2
-            //Se pide número a validar, por defecto se pone 1, aunque se podría validar ingreso del usuario
-            Console.WriteLine("Ingrese un entero para validar si cumple con la sucesión de Fibonacci (1 por defecto): ");
-            int num = int.TryParse(Console.ReadLine(), out num) ? num : 1;   
-            
-            string validateFibonacci = isFibonacci(num);
-            Console.WriteLine(validateFibonacci);
-
-            Console.ReadLine();
-
-
-            // PUNTO 1
-            // Se pide al usuario la cadena de texto
-            Console.WriteLine("Ingrese una cadena de texto: ");
-            string completeText = Console.ReadLine();
-
-            // Se solicita la cántidad máxima de caracteres para realizar el recorte, si no se da ningún valor, por defect es 20
-            Console.WriteLine("Ingrese una cantidad máxima de caracteres(20 por defecto): ");                    
-            int maxChars = int.TryParse(Console.ReadLine(), out maxChars) ? maxChars : 20;
-
-            string trimText = TrimString(completeText, maxChars);
-            Console.WriteLine(trimText);
-
-
-            
-
-            Console.ReadLine();
+            return (numArray, totalCount);        
+                       
 
         }
 
         private static string TrimString(string completeText, int maxChars)
         {
             return completeText.Length <= maxChars ? completeText : completeText.Substring(0, maxChars) + "...";
-
 
         }
 
